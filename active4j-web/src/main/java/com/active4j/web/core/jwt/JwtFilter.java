dbd21c8541b50c34a772f7b41564f20fbcdac61e
shiheng.fsh <shiheng.fsh@alibaba-inc.com>
2020-12-18 14:07:55 +0800

    Overwrite Pom File， Push Goods Code 一个基本的物品属性展示与增删改查

--- active4j-web/src/main/java/com/active4j/web/core/jwt/JwtFilter.java
+++ active4j-web/src/main/java/com/active4j/web/core/jwt/JwtFilter.java
@@ -7,13 +7,13 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.active4j.entity.base.model.ResultJson;
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,6 +30,7 @@ public class JwtFilter extends BasicHttpAuthenticationFilter {
 	 */
 	@Override
 	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+//		return true;
 			try {
 				executeLogin(request, response);
 				return true;
